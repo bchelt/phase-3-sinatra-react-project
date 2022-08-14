e461bf5a7a9bdf56130b78904f626793fc92dd28
Brandon Helt <brandon.helt@yahoo.com>
2022-08-13 21:28:03 -0700

    Completed Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/todos" do
+    Todo.all.to_json(include: { category: { only: [:name] } })
+  end
+
+  get "/categories" do 
+    Category.all.to_json
+  end
+
+  post "/todos" do
+    todo = Todo.create(task: params[:task], category_id: params[:category_id])
+    todo.to_json(include: { category: { only: [:name] } })
+  end
+
+  patch "/todos/:id" do
+    todo = Todo.find(params[:id])
+    todo.update(task: params[:task], category_id: params[:category_id])
+    todo.to_json(include: { category: { only: [:name] } })
+  end
+
+  delete "/todos/:id" do
+    todo = Todo.find(params[:id])
+    todo.destroy
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+c1 = Category.create(name: "Shopping")
+c2 = Category.create(name: "Cleaning")
+c3 = Category.create(name: "Misc")
+
+t1 = Todo.create(task: "Take out the trash", category_id: c2.id)
+t2 = Todo.create(task: "Get bananas", category_id: c1.id)
+t3 = Todo.create(task: "Clean room", category_id: c2.id)
+t4 = Todo.create(task: "Create todo application", category_id: c3.id)
 
 puts "✅ Done seeding!"
